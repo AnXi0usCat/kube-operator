de5ffef18e43b5574ca3040e799ae5e2169f3e30
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2025-11-02 11:53:59 +0000

    create a deployment

--- operator/src/reconsile.rs
+++ operator/src/reconsile.rs
@@ -2,8 +2,13 @@ use std::{collections::BTreeMap, sync::Arc, time::Duration};
 
 use crate::crd::ModelDeployment;
 use k8s_openapi::{
-    api::core::v1::{Service, ServicePort, ServiceSpec},
-    apimachinery::pkg::util::intstr::IntOrString,
+    api::{
+        apps::v1::{Deployment, DeploymentSpec, DeploymentStrategy, RollingUpdateDeployment},
+        core::v1::{
+            Container, ContainerPort, PodSpec, PodTemplateSpec, Service, ServicePort, ServiceSpec,
+        },
+    },
+    apimachinery::pkg::{apis::meta::v1::LabelSelector, util::intstr::IntOrString},
 };
 use kube::Error as KubeError;
 use kube::{
@@ -52,5 +57,62 @@ async fn ensure_service(api: &Api<Service>, svc_name: &str, app_name: &str) -> R
 
     api.create(&PostParams::default(), &service).await?;
     println!("Created service {}", svc_name);
+    Ok(())
+}
+
+async fn ensure_deployment(
+    api: &Api<Deployment>,
+    name: &str,
+    image: &str,
+    replicas: i32,
+) -> Result<(), Error> {
+    if api.get_opt(name).await?.is_some() {
+        return Ok(());
+    }
+
+    let labels = BTreeMap::from([("app".into(), name.into())]);
+    let container = Container {
+        name: name.into(),
+        image: Some(image.into()),
+        ports: Some(vec![ContainerPort {
+            container_port: 8000,
+            ..Default::default()
+        }]),
+        ..Default::default()
+    };
+
+    let deploy = Deployment {
+        metadata: ObjectMeta {
+            name: Some(name.into()),
+            labels: Some(labels.clone()),
+            ..Default::default()
+        },
+        spec: Some(DeploymentSpec {
+            replicas: Some(replicas),
+            selector: LabelSelector {
+                match_labels: Some(labels.clone()),
+                ..Default::default()
+            },
+            template: PodTemplateSpec {
+                metadata: Some(ObjectMeta {
+                    labels: Some(labels.clone()),
+                    ..Default::default()
+                }),
+                spec: Some(PodSpec {
+                    containers: vec![container],
+                    ..Default::default()
+                }),
+            },
+            strategy: Some(DeploymentStrategy {
+                rolling_update: Some(RollingUpdateDeployment::default()),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }),
+        ..Default::default()
+    };
+
+    api.create(&PostParams::default(), &deploy).await?;
+    println!("created Deployment: {}", name);
     Ok(())
 }
